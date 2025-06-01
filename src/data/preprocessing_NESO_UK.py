91ab872cc0f3aaf5a08de03ef5e51034a73904fa
antonin310 <antonin.moreno1@outlook.fr>
2025-06-01 15:18:25 +0100

    feat(preprocessing): drop redundant cols, ensuring consistency in the cyclic encoding, and scale numeric features - drop raw flow, embedded generation, regional demand, ND, TSD, pump storage, and settlement period - Rounded sin/cos encoding to get exact zeros or ones where expected - drop original hour, weekday, and month columns - standard scale the features

--- src/data/preprocessing_NESO_UK.py
+++ src/data/preprocessing_NESO_UK.py
@@ -10,29 +10,53 @@ df = pd.read_csv(
 )
 
 
+# Drop columns that become redundant after adding the new features
+df.drop(columns=[
+    'I014_FRENCH_FLOW', 'I014_BRITNED_FLOW', 'I014_MOYLE_FLOW', 'I014_EAST_WEST_FLOW',  # individual flows
+    'FRENCH_FLOW', 'BRITNED_FLOW', 'MOYLE_FLOW', 'EAST_WEST_FLOW',                       
+    'EMBEDDED_WIND_GENERATION', 'EMBEDDED_WIND_CAPACITY',                               # raw wind data
+    'EMBEDDED_SOLAR_GENERATION', 'EMBEDDED_SOLAR_CAPACITY',                             # raw solar data
+    'ENGLAND_WALES_DEMAND',                                                             # regional demand
+    'ND', 'TSD', 'I014_TSD',                                                            # national demand
+    'PUMP_STORAGE_PUMPING',                                                                
+    'SETTLEMENT_PERIOD'                                                                                            
+                                                                                            
+], inplace=True)
+
+
 # Cyclical encoding of temporal features
-df['hour_sin']    = np.sin(2 * np.pi * df['hour']   / 24)
-df['hour_cos']    = np.cos(2 * np.pi * df['hour']   / 24)
+df['hour_sin'] = np.sin(2 * np.pi * df['hour']   / 24)
+df['hour_sin'] = np.round(df['hour_sin'], 14)   # Rounding to get exact zeros or ones where expected
+
+df['hour_cos'] = np.cos(2 * np.pi * df['hour']   / 24)
+df['hour_cos'] = np.round(df['hour_cos'], 14)   # Rounding to get exact zeros or ones where expected
+
 df['weekday_sin'] = np.sin(2 * np.pi * df['weekday']/ 7)
 df['weekday_cos'] = np.cos(2 * np.pi * df['weekday']/ 7)
 
 # subtract 1 so months (1–12) map to 0–11 and December sits just before January on the circle
-df['month_sin']   = np.sin(2 * np.pi * (df['month']-1)/ 12)
-df['month_cos']   = np.cos(2 * np.pi * (df['month']-1)/ 12)
+df['month_sin'] = np.sin(2 * np.pi * (df['month']-1)/ 12)
+df['month_sin'] = np.round(df['month_sin'], 14)    # Rounding to get exact zeros or ones where expected
 
-# Standard scaling of numeric features
-numeric_feats = df.select_dtypes(include=[np.number]).columns.tolist()
-exclude = ['hour_sin', 'hour_cos', 'weekday_sin', 'weekday_cos', 'month_sin', 'month_cos', 'ND', 'I014_ND', 'TSD', 'I014_TSD']
-numeric_feats = [c for c in numeric_feats if c not in exclude]
+df['month_cos'] = np.cos(2 * np.pi * (df['month']-1)/ 12)
+df['month_cos'] = np.round(df['month_cos'], 14)    # Rounding to get exact zeros or ones where expected
 
 
+# Drop hour, month and weekday 
+df.drop(columns=['weekday', 'month', 'hour'], inplace=True)
+
+# Standard scaling of features
+features = ['NON_BM_STOR','I014_PUMP_STORAGE_PUMPING','I014_ND_lag_1','I014_ND_lag_2',
+            'I014_ND_lag_48','I014_ND_lag_96','I014_ND_lag_336','I014_ND_mean_48',
+            'I014_ND_mean_336','net_import','wind_capacity_factor','solar_capacity_factor'
+]
 
 scaler = StandardScaler()   # z-score normalization
 
 # Fit the scaler on the selected numeric features and transform them
-df[numeric_feats] = scaler.fit_transform(df[numeric_feats])
+df[features] = scaler.fit_transform(df[features])
 
-
+df.to_csv('D:/Data science/energy-forecasting-ml/data/processed/NESO_UK/preprocessed_new_features_cleaned_DemandData_2011-2018.csv')
 
 
 
